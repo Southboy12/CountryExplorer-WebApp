6176571277e49212f2116ae556e223b0112d067f
Southboy <menkachi85@gmail.com>
2024-04-27 01:12:49 +0100

    Project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function Navbar() {
+    return (
+        <div>
+            Navbar goes here
+        </div>
+    )
+}
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function Search() {
+    return (
+        <h1>
+            Search goes here
+        </h1>
+    )
+}
