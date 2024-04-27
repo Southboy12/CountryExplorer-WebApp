04e58200cfa2339e06a599ad0124198a8fcb055b
Southboy <menkachi85@gmail.com>
2024-04-27 03:05:17 +0100

    Implemented navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import React from 'react'
-
-
+import React from "react";
+import Logo from "../images/moon-outline.svg"
 
 export default function Navbar() {
-    return (
-        <div>
-            Navbar goes here
+  return (
+    <nav className="nav--body">
+        <div className="container">
+            <h3 className="nav--title">Where in the world?</h3>
+            <div className="nav--darkmode">
+                <img className="nav--logo" src={Logo} alt="dark mode logo" />
+                <p className="logo--text">Dark Mode</p>
+            </div>
         </div>
-    )
+    </nav>
+  );
 }
