e66e0597cf4a8c0440622eba75a615a6234235d2
Southboy <menkachi85@gmail.com>
2024-04-27 05:27:02 +0100

    Implemented the Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import searchIcon from '../images/search.png'
 
 
 
 export default function Search() {
     return (
-        <h1>
-            Search goes here
-        </h1>
+        <div className='main--container'>
+            <div className='search--container'>
+                <input 
+                    className='search--input'
+                    type='text'
+                    placeholder='Search for a country...'
+                    name='search'
+                />
+                <img
+                    className='search--icon'
+                    src={searchIcon}
+                    alt='search icon'
+                />
+            </div>
+            <select className='dropdown'>
+                <option selected>Filter by Region</option>
+                <option>Africa</option>
+                <option>America</option>
+                <option>Asia</option>
+                <option>Europe</option>
+                <option>Oceania</option>
+            </select>
+        </div>
     )
 }
